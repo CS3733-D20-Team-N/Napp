3106c30b6ef5377e1cdb1fd7abf95d6ede921afe
Matt Johannesen <matt.d.johannesen@gmail.com>
2020-04-04 14:46:46 -0400

    Fixed some build-breaking bugs in Matt's window

--- src/main/java/edu/wpi/N/views/MattController.java
+++ src/main/java/edu/wpi/N/views/MattController.java
@@ -1,6 +1,7 @@
 package edu.wpi.N.views;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,15 +9,15 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.input.MouseEvent;
-import javafx.event.ActionEvent;
-import javafx.stage.Stage;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 public class MattController {
 
   @FXML Button btn_goToMain;
   @FXML ColorPicker colorPicker;
 
+  @FXML
   private void handleOnClick(MouseEvent event) throws IOException {
     Stage stage;
     Parent root;
@@ -29,8 +30,6 @@ public class MattController {
 
   public void handle(ActionEvent event) throws IOException {
     Color c = colorPicker.getValue();
-
-    System.out.println("New Color's RGB = "+c.getRed()+" "+c.getGreen()+" "+c.getBlue())
+    System.out.println("New Color's RGB = " + c.getRed() + " " + c.getGreen() + " " + c.getBlue());
   }
-
 }
